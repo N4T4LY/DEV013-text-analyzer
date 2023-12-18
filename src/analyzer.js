799665cdb57186f6e8c43b5ffb23e258603b451f
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2023-12-18 13:02:16 -0400

    comentarios y arreglos finales analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,77 +1,89 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
 
-    if(!text.trim()){
+    if (!text.trim()) {
       return 0;
     }
-    let tam= text.split(" ");
-    
+    //divido las cadenas en base a espacios y las almaceno en tam
+    let tam = text.split(" ");
+
     return tam.length;
 
-    
+
 
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
+    //verifico si en el textarea hay espacios o la cadena esta vacia y retorna 0 si es el caso
     if (!text.trim()) {
       return 0;
-  }
+    }
 
+    //calculo el tamano de la cadena y la envio como resultado  
     return text.length;
-    
-  
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
 
     if (!text.trim()) {
       return 0;
-  }
-
+    }
+    //ingreso los caracteres especiales y espacios que quiero borrar y los reemplazo en la cadena con cadenas con una cadena vacia y despues calculo el tamaño de la cadena resultante
     return text.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
 
- if(!text.trim()){
+    if (!text.trim()) {
       return 0;
     }
-    
-    const palabras=text.split(" ");
-    const tam=palabras.length
-    let suma=0;
-    
 
-    for(let i=0;i<tam;i++){
-     
-      suma+=palabras[i].length;
-      
-  }
-  let longitud=(suma/palabras.length).toFixed(2);
+    //divido las palabras de acuerdo al espacio entre cada una
+    const palabras = text.split(" ");
+    //calculo el numero de las palabras que se fueron encontrando
+    const tam = palabras.length
+    //inicializo la variable en donde se almacena la suma de la longitud de cada palabra
+    let suma = 0;
 
-    return longitud; 
+    //con un for recorro el array de todas las palabras que se fueron desplegando con el split
+    for (let i = 0; i < tam; i++) {
 
-   
+      //tomo la palabra en la posicion i y tomo su longitud y luego la sumo en la variable suma
+      suma += palabras[i].length;
+
+    }
+    //inicializo la variable longitud, divido la suma obtenida con el for con el numero de las palabras, con el tofixed(2) convierto el valor int en un float y el con el 2 especifico cuantos decimales quiero despues de la coma
+    let longitud = (suma / tam).toFixed(2);
+
+    return longitud;
+
+
 
   },
 
-  
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    if (!text.trim() ) {
+    if (!text.trim()) {
       return 0;
     }
-    let resultado=text.match(/\d+/g);
-    if(resultado===null){
+
+    //inicializo la variable resultado y voy almacenando los numeros que se encuentran en la cadena, con el + especifico que sean digitos consecutivos para que los tome en cuenta como numeros y no digitos aparte
+    let resultado = text.match(/\d+/g);
+    //si no existen numeros retorna 0
+    if (resultado === null) {
       return 0;
-    }else{
+    } else {
+      //y si existen numeros devuelvo el numero de elementos que se encontraron
       return resultado.length;
- 
+
 
     }
-    
-    
+
+
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
@@ -79,20 +91,27 @@ const analyzer = {
       return 0;
     }
 
-    let suma=0;
-    let numero=text.match(/\d+/g);
+    //inicializo variable suma
+    let suma = 0;
+    //busco los numeros y los almaceno en numero
+    let numero = text.match(/\d+/g);
     
-    if(numero){
-      for(let i=0;i<numero.length;i++){
-        suma+= parseInt(numero[i],10);
-        
-        
-    }
-    }else{
+    //pregunto si hay o no numeros retorna 0 si no
+    if (numero === null) {
       return 0;
+    } else {
+       //recorro el array numero     
+        for (let i = 0; i < numero.length; i++) {
+          //en la variable suma voy sumando los numeros, con parseint los convierto a enteros en base 10
+          suma += parseInt(numero[i], 10);
+
+        }
+     
+      return suma;
     }
-    return suma;
-  
+
+
+
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -11,22 +11,12 @@ refresh.addEventListener('click', _ => {
 //llamo el contenido del textarea con query selector
 let textarea = document.querySelector('[name="user-input"]');
 
-//capturo li de num caracteres
+//con el queryselector capturo el li de cada elemento para despues mostrar el resultado de cada uno
 let mostrarnumcaracteres = document.querySelector('[data-testid="character-count"]');
-
-//capturo el li de caracteres sin espacio
 let mostrarnumcaracteresse=document.querySelector('[data-testid="character-no-spaces-count"]');
-
-//capturo el li de num palabras
 let mostrarnumpalabras=document.querySelector('[data-testid="word-count"]');
-
-//capturo el li de num de numeros
 let mostrarnumnumeros=document.querySelector('[data-testid="number-count"]');
-
-//capturo el li de suma de numeros
 let mostrarsuma=document.querySelector('[data-testid="number-sum"]');
-
-//capturo el li de longitud
 let mostrarlongitud=document.querySelector('[data-testid="word-length-average"]');
 
 //funcion, con addevenlistener escucho el valor del textarea y ejecuto las funciones del analyzer
@@ -35,10 +25,11 @@ textarea.addEventListener('input',()=>{
     //capturo el valor de textarea y lo almaceno en text
     let text =textarea.value;
 
-    //creo una variable donde almaceno el resultado del la funcion getwordcount y la despliego en su li
+    //creo una variable donde almaceno el resultado de las funciones mandando el valor del textarea y la despliego en su li respectivo con el textcontent
     let contarpalabras=analyzer.getWordCount(text);
     mostrarnumpalabras.textContent=`Recuento palabras: ${contarpalabras}`
 
+    
     let contarcaracteres=analyzer.getCharacterCount(text);
     mostrarnumcaracteres.textContent=`Recuento caracteres: ${contarcaracteres}`;
 
@@ -53,7 +44,7 @@ textarea.addEventListener('input',()=>{
     mostrarsuma.textContent=`Suma de números: ${sumar}`
 
     let longitudmedia=analyzer.getAverageWordLength(text);
-    mostrarlongitud.textContent=`Longitud: ${longitudmedia}`
+    mostrarlongitud.textContent=`Promedio de Longitud: ${longitudmedia}`
 
 
 
