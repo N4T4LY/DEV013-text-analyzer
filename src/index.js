83cf6f28e25933f505959d94ff0375908be5e7e6
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2023-12-17 20:00:15 -0400

    arreglo htmlycss, conteocaractersinespacio, conteopalabras

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import analyzer from './analyzer.js';
 
+//recargo la pagina y limpio el contenido
+let refresh = document.getElementById('reset-button');
+refresh.addEventListener('click', _ => {
+            location.reload();
+})
+
+//
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//llamo el contenido del textarea con query selector
+let textarea = document.querySelector('[name="user-input"]');
+//
+let mostrarnumcaracteres=document.querySelector('[data-testid="character-no-spaces-count"]');
+
+let mostrarnumpalabras=document.querySelector('[data-testid="word-count"]');
+textarea.addEventListener('input',()=>{
+    let text =textarea.value;
+
+    let contarpalabras=analyzer.getWordCount(text);
+    mostrarnumpalabras.textContent=`Recuento palabras: ${contarpalabras}`
+
+
+    let contarcaracteres=analyzer.getCharacterCountExcludingSpaces(text);
+    mostrarnumcaracteres.textContent=`Recuento caracteres sin espacio: ${contarcaracteres} `;
+});
