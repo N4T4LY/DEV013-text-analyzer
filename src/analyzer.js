8b83da154308d9708fff7c8c99b0bd09f7ab30ee
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2023-12-17 22:20:40 -0400

    conteonumeros, sumanumeros

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,33 +1,55 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
     if(!text.trim()){
       return 0;
     }
     let tam= text.split(" ");
-    let numero=tam.length;
-    return numero;
+    
+    return tam.length;
 
     
 
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
+    return text.length;
     
   
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    return text.replace(/\s/g, '').length;
+
+    return text.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').length;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let resultado=text.match(/\d+/g);
+    return resultado.length;
+ 
+    
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let suma=0;
+    let numero=text.match(/\d+/g);
+    //console.log(numero);
+    if(numero){
+      for(let i=0;i<numero.length;i++){
+        suma+= parseInt(numero[i],10);
+        //console.log(suma);
+        
+    }
+    }else{
+      return 0;
+    }
+    return suma;
+  
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import analyzer from './analyzer.js';
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 //recargo la pagina y limpio el contenido
 let refresh = document.getElementById('reset-button');
@@ -6,22 +7,47 @@ refresh.addEventListener('click', _ => {
             location.reload();
 })
 
-//
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 //llamo el contenido del textarea con query selector
 let textarea = document.querySelector('[name="user-input"]');
-//
-let mostrarnumcaracteres=document.querySelector('[data-testid="character-no-spaces-count"]');
 
+//capturo li de num caracteres
+let mostrarnumcaracteres = document.querySelector('[data-testid="character-count"]');
+
+//capturo el li de caracteres sin espacio
+let mostrarnumcaracteresse=document.querySelector('[data-testid="character-no-spaces-count"]');
+
+//capturo el li de num palabras
 let mostrarnumpalabras=document.querySelector('[data-testid="word-count"]');
+
+//capturo el li de num de numeros
+let mostrarnumnumeros=document.querySelector('[data-testid="number-count"]');
+
+//capturo el li de suma de numeros
+let mostrarsuma=document.querySelector('[data-testid="number-sum"]');
+
+//funcion, con addevenlistener escucho el valor del textarea y ejecuto las funciones del analyzer
 textarea.addEventListener('input',()=>{
+    
+    //capturo el valor de textarea y lo almaceno en text
     let text =textarea.value;
 
+    //creo una variable donde almaceno el resultado del la funcion getwordcount y la despliego en su li
     let contarpalabras=analyzer.getWordCount(text);
     mostrarnumpalabras.textContent=`Recuento palabras: ${contarpalabras}`
 
+    let contarcaracteres=analyzer.getCharacterCount(text);
+    mostrarnumcaracteres.textContent=`Recuento caracteres: ${contarcaracteres}`;
 
-    let contarcaracteres=analyzer.getCharacterCountExcludingSpaces(text);
-    mostrarnumcaracteres.textContent=`Recuento caracteres sin espacio: ${contarcaracteres} `;
+
+    let contarcaracteresse=analyzer.getCharacterCountExcludingSpaces(text);
+    mostrarnumcaracteresse.textContent=`Recuento caracteres sin espacio: ${contarcaracteresse} `;
+
+    let contarnumeros=analyzer.getNumberCount(text);
+    mostrarnumnumeros.textContent=`Recuento numeros: ${contarnumeros}`;
+
+    let sumar=analyzer.getNumberSum(text);
+    mostrarsuma.textContent=`Suma de números: ${sumar}`
+
+
 });
