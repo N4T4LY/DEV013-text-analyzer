ae0c2268527bc79f41cc9b3a11262a5fc30f936f
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2023-12-20 17:22:14 -0400

    arreglo testing css

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,30 +1,23 @@
 const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    if(text===null){
+    
+    if (!text.trim()||text===null) {
       return 0;
     }
-    if (!text.trim()) {
-      return 0;
-    }
-  
     let tam = text.split(" ");
-
     return tam.length;
-
-
-
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
 
     if(text===null){
       return 0;
     }
-
-    //calculo el tamano de la cadena y la envio como resultado  
     return text.length;
   },
+
 
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
@@ -32,46 +25,32 @@ const analyzer = {
     if (!text.trim()) {
       return 0;
     }
-    //ingreso los caracteres especiales y espacios que quiero borrar y los reemplazo en la cadena con cadenas con una cadena vacia y despues calculo el tamaño de la cadena resultante
     return text.replace(/[\s.,/#!$%^&*;:{}=\-_`~()]/g, "").length;
   },
+
+  
   getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
 
     if (!text.trim()) {
       return 0;
     }
-
-    //divido las palabras de acuerdo al espacio entre cada una
     const palabras = text.split(" ");
-    //calculo el numero de las palabras que se fueron encontrando
     const tam = palabras.length;
-    //inicializo la variable en donde se almacena la suma de la longitud de cada palabra
     let suma = 0;
-
-    //con un for recorro el array de todas las palabras que se fueron desplegando con el split
     for (let i = 0; i < tam; i++) {
-
-      //tomo la palabra en la posicion i y tomo su longitud y luego la sumo en la variable suma
       suma += palabras[i].length;
-
     }
-    //inicializo la variable longitud, divido la suma obtenida con el for con el numero de las palabras, con el tofixed(2) convierto el valor int en un float y el con el 2 especifico cuantos decimales quiero despues de la coma
     let longitud = parseFloat((suma / tam).toFixed(2));
-
     return longitud;
-
-
 
   },
 
 
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    if(text===null){
-      return 0;
-    }
-    if (!text.trim()) {
+    
+    if (!text.trim()||text===null) {
       return 0;
     }
     let palabras=text.split(" ");
@@ -86,17 +65,13 @@ const analyzer = {
       
     }
     return contar;
-
-    
-
-
   },
+
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    if(text===null){
-      return 0;
-    }
-    if (!text.trim()) {
+   
+    if (!text.trim()||text===null) {
       return 0;
     }
     let palabras=text.split(" ");
@@ -110,9 +85,7 @@ const analyzer = {
         }else{
           suma+=parseInt(palabras[i]);          
         }
-        
       }
-      
     }
     
     console.log(suma);
