43e377de1d627a3ef5266b9c8ca9dd43339588c8
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2023-12-21 22:16:42 -0400

    mejoras codigo

--- src/analyzer.js
+++ src/analyzer.js
@@ -3,29 +3,50 @@ const analyzer = {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
     
     if (!text.trim()||text===null) {
+      //console.log("0");
       return 0;
     }
     let tam = text.split(" ");
-    return tam.length;
+    let contar=0;
+    for (let i = 0; i < tam.length; i++) {
+      if(tam[i].match(/\w/g)){
+        contar=contar+1;
+        //console.log(tam[i].match(/\w/g));
+      }
+    }
+    if(contar===0){
+     
+      return 0;
+    }
+   
+    return contar;
   },
+
 
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
 
     if(text===null){
+    // console.log(0);
       return 0;
     }
+    //console.log(text.length);
     return text.length;
   },
 
 
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-
     if (!text.trim()) {
-      return 0;
+      return "0" ;
     }
-    return text.replace(/[\s.,/#!$%^&*;:{}=\-_`~()]/g, "").length;
+    const resultado = text.replace(/[\s.,/#"«»!¡¿?$%^&*;:{}=[\-_`~()\]]/g, "").length;
+    if(resultado===null){
+      return "0";
+    }
+    console.log(resultado);
+    return resultado;
+    
   },
 
   
@@ -62,7 +83,6 @@ const analyzer = {
       if(palabras[i].match(/\b\d+\b/g)){
         contar=contar+1;
       }
-      
     }
     return contar;
   },
@@ -80,15 +100,19 @@ const analyzer = {
       if(palabras[i].match(/\b\d+\b/g)){
         if(palabras[i].match(/\b\d+(\.\d+)?\b/g)){
           suma+=parseFloat(palabras[i]);
-
-
         }else{
           suma+=parseInt(palabras[i]);          
         }
       }
     }
-    
-    console.log(suma);
+
+    if(suma % 1 !== 0){
+      suma=parseFloat(suma.toFixed(2));
+
+    }else{
+      suma=parseInt(suma);
+    }
+          
     return suma;
 
    
